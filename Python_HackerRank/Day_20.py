e6f6cccd86978d57ea24dad5844ef9aba4bd3b1b
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-09 13:25:50 -0500

    final version

--- Python_HackerRank/Day_20.py
+++ Python_HackerRank/Day_20.py
@@ -1,23 +1,21 @@
 import sys
 
+
+def bubble_sort(lista):
+    n = len(lista)
+    Swaps = 0
+    for i in range(n):
+        for j in range(0, n - i - 1):
+
+            if lista[j] > lista[j + 1]:
+                lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                Swaps = Swaps + 1 
+    print('Array sorted in',Swaps, 'swaps.')
+    print('First Element:', lista[0])
+    print('Last Element:', lista[len(lista) - 1])
+
 n = int(input().strip())
 a = list(map(int, input().strip().split(' ')))
 # Write Your Code Here
 
-for (int i = 0; i < n; i++) {
-    // Track number of elements swapped during a single array traversal
-    int numberOfSwaps = 0;
-    
-    for (int j = 0; j < n - 1; j++) {
-        // Swap adjacent elements if they are in decreasing order
-        if (a[j] > a[j + 1]) {
-            swap(a[j], a[j + 1]);
-            numberOfSwaps++;
-        }
-    }
-    
-    // If no elements were swapped during a traversal, array is sorted
-    if (numberOfSwaps == 0) {
-        break;
-    }
-}
+bubble_sort(a)
