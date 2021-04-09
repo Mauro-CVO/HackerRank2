b32efc8a5a53a6f2696ae8c9a18fef322b293b74
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-09 14:00:10 -0500

    primera version

---
+++ Python_HackerRank/Project_Euler1.py
@@ -0,0 +1,28 @@
+import sys
+
+
+def multiplos(lista):
+    multip_lst = []
+
+    for i in lst:
+        if i%3 == 0 or i%5 == 0:
+            multip_lst.append(i)
+    
+
+
+
+
+
+
+
+
+
+t = int(input().strip())
+lst = []
+for a0 in range(t):
+    lst.append(a0)
+    #n = int(input().strip())
+
+multiplos(lst)
+
+
