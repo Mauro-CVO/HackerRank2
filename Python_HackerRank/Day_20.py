0bbee851758b67e53db0a6e63ddb0c1b34c12f1a
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-09 12:50:25 -0500

    1ra version

---
+++ Python_HackerRank/Day_20.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(input().strip())
+a = list(map(int, input().strip().split(' ')))
+# Write Your Code Here
+
+for (int i = 0; i < n; i++) {
+    // Track number of elements swapped during a single array traversal
+    int numberOfSwaps = 0;
+    
+    for (int j = 0; j < n - 1; j++) {
+        // Swap adjacent elements if they are in decreasing order
+        if (a[j] > a[j + 1]) {
+            swap(a[j], a[j + 1]);
+            numberOfSwaps++;
+        }
+    }
+    
+    // If no elements were swapped during a traversal, array is sorted
+    if (numberOfSwaps == 0) {
+        break;
+    }
+}
