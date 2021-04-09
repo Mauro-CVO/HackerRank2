b370efb6bfb07b959af6af988b6d39dade6f7d5c
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-08 20:23:13 -0500

    version final, correcion de errores

--- Python_HackerRank/Day_18.py
+++ Python_HackerRank/Day_18.py
@@ -10,7 +10,7 @@ def palindrome(p):
         s_ls.append(s_ws[j])
         i = i - 1
         j = j + 1
-        #print(s_ls)
+        print(s_ls)
 
     
     s_ls1 = []
@@ -19,29 +19,22 @@ def palindrome(p):
         s_ls1.append(i)
     for i in s_ls[len(s_ls) - len(s_ls)//2:]:
         s_ls2.append(i)
-    k = len(s_ls1) - 1
+    k = 0
     n = 0
     m = len(s_ls2) - 1
-    while k != 0:
-        print(s_ls1[n])
-        print(s_ls2[m])
-        print(k)
+    print(s_ls1[n])
+    print(s_ls2[m])
+    for i in s_ls1:
         if s_ls1[n] == s_ls2[m]:
             n = n + 1
             m = m - 1
+            k = k + 1
         else:
-            raise Exception ("The word, "+s+", is not a palindrome.")
-        k = k - 1
-    print("The word, "+s+", is a palindrome.")
-'''
-    else:
-        s_ls1 = []
-        s_ls2 = []
-        for i in s_ls[:len(s_ls)/2]:
-            s_ls1.append(i)
-        for i in s_ls[len(s_ls) - len(s_ls)/2:]:
-            s_ls2.append(i)
-'''
+            print ("The word, "+s+", is not a palindrome.")
+            break
+        if k == len(s_ls1):
+            print("The word, "+s+", is a palindrome.")
+
 
 s = input()
 palindrome(s)
