f0112e748c52431f7d10eaf5a6e298676763951d
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-08 19:28:49 -0500

    palabra puesta en una lista

--- Python_HackerRank/Day_18.py
+++ Python_HackerRank/Day_18.py
@@ -3,11 +3,16 @@ def palindrome(p):
     s_ls = []
     s_ws = s.replace(' ','')
     i = len(s_ws)
+    j = 0
     #print(s_ws)
 
     while i != 0:
+        s_ls.append(s_ws[j])
+        i = i - 1
+        j = j + 1
+        print(s_ls)
 
-
+    print(s_ls)
 
 
 s = input()
