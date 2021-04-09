500dcbc57d228bb409278111081002bf53286dbd
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-08 20:00:05 -0500

    detecta oraciones/palabras impares

--- Python_HackerRank/Day_18.py
+++ Python_HackerRank/Day_18.py
@@ -10,9 +10,30 @@ def palindrome(p):
         s_ls.append(s_ws[j])
         i = i - 1
         j = j + 1
-        print(s_ls)
+        #print(s_ls)
 
-    print(s_ls)
+    if len(s_ls)%2 != 0:
+        s_ls1 = []
+        s_ls2 = []
+        for i in s_ls[:len(s_ls)//2]:
+            s_ls1.append(i)
+        for i in s_ls[len(s_ls) - len(s_ls)//2:]:
+            s_ls2.append(i)
+        k = len(s_ls1) - 1
+        n = 0
+        m = len(s_ls2) - 1
+        while k != 0:
+            print(s_ls1[n])
+            print(s_ls2[m])
+            print(k)
+            if s_ls1[n] == s_ls2[m]:
+                n = n + 1
+                m = m - 1
+            else:
+                raise Exception ("The word, "+s+", is not a palindrome.")
+            k = k - 1
+        print("The word, "+s+", is a palindrome.")
+
 
 
 s = input()
