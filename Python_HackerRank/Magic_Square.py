5ac369c1b881cac0177cae783e2d5cc5629f0654
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-21 00:18:12 -0500

    version final

--- Python_HackerRank/Magic_Square.py
+++ Python_HackerRank/Magic_Square.py
@@ -9,24 +9,6 @@ def formingMagicSquare(s):
     m8 = [[2,7,6],[9,5,1],[4,3,8]]
     posibilities = [m1,m2,m3,m4,m5,m6,m7,m8]
     
-    #equals = []
-
-    '''for matrix in posibilities:
-        equal = 0
-        for i in range(len(s)):
-            for j in range(len(s)):
-                if s[i][j] == matrix[i][j]:
-                    equal += 1
-        equals.append(equal)
-    print('equals:',equals)
-    index = equals.index(max(equals))
-    print(index)
-    positions =[]'''
-    
-    '''for i in range(len(equals)):
-        if equals[i] == equals[index]:
-            positions.append(i)
-    print('positions:',positions)'''
     costs = []
 
     for matrix in posibilities:
