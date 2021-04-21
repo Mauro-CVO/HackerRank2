efa6a4d8fd78a318f88501e2eac7574e0293d810
Mauro-CVO <mauo.cortes2@gmail.com>
2021-04-20 21:56:25 -0500

    Counting-valleys, version final

--- Python_HackerRank/Counting_Valleys.py
+++ Python_HackerRank/Counting_Valleys.py
@@ -1,25 +1,21 @@
 def counting_valleys(steps, path):
-    sea_level = 1
-    y_plus = 0
-    y _minus= 0
-    way = 1
-    for i in range(len(path)):
-        if path[i] == 'U':
-            y_plus += 1
-            way = y_plus + way
-        elif path[i] == 'D':
-            y_minus -= 1
-            way = y_minus + way
+    sea_level = 0
+    valleys = 0
+    for i in path:
+        if i == 'U':
+            sea_level += 1
+        else:
+            sea_level -= 1
         
-        
-    print('x: ',x, 'y: ',y)
-    print(final)
-
+        if i == 'U' and sea_level == 0:
+            valleys += 1
+    print(valleys)
 
 def run():
     steps = int(input().strip())
-    path = input().strip()
-    
+
+    path = input()
+
     counting_valleys(steps, path)
 
 if __name__ == '__main__':
